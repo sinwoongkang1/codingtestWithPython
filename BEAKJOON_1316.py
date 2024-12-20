8913036b727f8c163d6bdd3be19795ff86679056
sinwoongkang1 <kkace22@gmail.com>
2024-12-21 00:09:39 +0900

    1 problem solved

--- BEAKJOON_1316.py
+++ BEAKJOON_1316.py
@@ -1,36 +1,11 @@
-import sys
-total = 0
-sumCount = 0
-
-for _ in range(20):
-    input = sys.stdin.readline().split()
-    time = float(input[1])
-    grade = str(input[2])
-    if grade == "A+":
-        total += (4.5 * time)
-        sumCount += time
-    elif grade == "A0":
-        total += (4 * time)
-        sumCount += time
-    elif grade == "B+":
-        total += (3.5 * time)
-        sumCount += time
-    elif grade == "B0":
-        total += (3 * time)
-        sumCount += time
-    elif grade == "C+":
-        total += (2.5 * time)
-        sumCount += time
-    elif grade == "C0":
-        total += (2 * time)
-        sumCount += time
-    elif grade == "D+":
-        total += (1.5 * time)
-        sumCount += time
-    elif grade == "D0":
-        total += (1 * time)
-        sumCount += time
-    elif grade == "F":
-        sumCount += time
-print(total/sumCount)
-    
+N = int(input())
+count = N
+for i in range(count):
+    inputWord = str(input())
+    for j in range(len(inputWord)-1):
+        if inputWord[j] == inputWord[j+1]:
+            continue
+        if inputWord[j] in inputWord[j+1:]:
+            count -= 1
+            break
+print(count)
---
+++ BEAKJOON_2563.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+array = [[0] * 100 for _ in range(100)]
+
+count = 0
+for i in range(N):
+    a,b = map(int,input().split())
+    for j in range(a,a+10,1):
+        for k in range(b,b+10,1):
+            if array[j][k] == 0 :
+                array[j][k] = 1
+                count += 1
+print(count)
+
+
+
+
+
+
+
