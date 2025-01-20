c31eedf3b325d22d967b1e82052a5736bae5fbcf
kang <kkace22@gmail.com>
2025-01-21 00:28:52 +0900

    1 problem edited

--- BEAKJOON_1380.py
+++ BEAKJOON_1380.py
@@ -4,15 +4,11 @@ while True:
     arr = []
     testCase = int(sys.stdin.readline().strip())
     if testCase == 0:
-        for i in range(len(answer)):
-            print(i+1,answer[i][0])
         break
     else:
         for i in range(testCase):
             name = str(sys.stdin.readline().strip())
             arr.append([name,0])
-      #   print(arr)
-      #   print(arr[0][1])
         for i in range(2*testCase -1):
             a ,b = map(str,sys.stdin.readline().split())
             a = int(a)            
@@ -20,7 +16,14 @@ while True:
                 arr[a-1][1] = b
             else:
                 if arr[a-1][1] != 0 and arr[a-1][1] != b:
-                  del arr[a-1]
-        answer.extend(arr)
+                  arr[a-1][1] = 1
+        for i in range(len(arr)):
+            if arr[i][1] != 1:
+                answer.append(arr[i][0])
+for i in range(len(answer)):
+    print(i+1,answer[i])
+        
 
+
+       
       
