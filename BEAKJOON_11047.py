bd23c95940b22132d96106b1dcf0011fa4584feb
kang <kkace22@gmail.com>
2025-01-21 21:41:39 +0900

    1 problem solved

---
+++ BEAKJOON_11047.py
@@ -0,0 +1,16 @@
+import sys
+N,K = map(int,sys.stdin.readline().split())
+stack = []
+for i in range(N):
+    a = int(sys.stdin.readline().strip())
+    if a <= K :
+        stack.append(a)
+stack = sorted(stack,reverse=True)
+count = 0
+for i in stack:
+    if K >= i:
+        count += K // i
+        K %= i
+        if K == 0:
+            break
+print(count)                 
