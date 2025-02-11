2b08d270258f65ede5e5bea03b02fabe7c3700f3
kang <kkace22@gmail.com>
2025-02-11 23:31:09 +0900

    time exceed

--- BEAKJOON_1874.py
+++ BEAKJOON_1874.py
@@ -2,41 +2,56 @@ import sys
 userInputLine = int(sys.stdin.readline().strip())
 
 stack = []
+stack_2 = []
 compareArray = []
 array = []
 brray = []
+signal = []
 
 for _ in range(userInputLine):
     userInputNumber = int(sys.stdin.readline().strip())
     stack.append(userInputNumber)
+    stack_2.append(userInputNumber)
     compareArray.append(userInputNumber)
 
 compareArray.sort()
 
 
 while True:
-    sing = None
-    if len(array) == 0:
+    if len(array) == 0 and len(compareArray) != 0:
         array.append(compareArray[0])
+        signal.append("+")
+    elif len(compareArray) == 0:
+        break
     else:
         if stack[0] != array[-1]:
             array.append(compareArray[0])
-    if array[-1] == stack[0]:
-        brray.append(array[-1])
-        del compareArray[0]
-        del stack[0]
-        del array[-1]
-        sign = "+"
-    else:
-        if array[-1] != stack[0]:
-            sign = "+"
-            del compareArray[0]
+            signal.append("+")
+    while True:
+        if len(array) == 0:
+            break
+        elif array[-1] == stack[0]:
+            brray.append(array[-1])
+            if array[-1] in compareArray:
+                del compareArray[0]
+            del stack[0]
+            del array[-1]
+            signal.append("-")
         else:
-            sing = "none"
-    if sing == "none":
-        break
-    else:
-        print(sign)
+            if array[-1] != stack[0]:
+                if array[-1] in compareArray:
+                    del compareArray[0]
+                break
+            else:
+                sing = "none"
+                break
+ 
+if stack_2 == brray:
+    print(*signal,sep="\n")
+else:
+    print("NO")
+
+
 
 
 
